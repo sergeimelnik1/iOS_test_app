7daa9fa627809b28e5cc291e2f9ad19e6e327524
sergeimelnik1 <sergeimelnik-1@yandex.ru>
2022-03-22 18:29:05 +0300

    Обновление
    Добавил описание методов touchesBegan и touchesEnded из учебника 8

--- sec8. Game/sec8. Game/GameScene.swift
+++ sec8. Game/sec8. Game/GameScene.swift
@@ -9,32 +9,80 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    //вызывается при первом запуске сцены
+    // вызывается при первом запуске сцены
     override func didMove(to view: SKView) {
-        //цвет фона сцены
+        // цвет фона сцены
         backgroundColor = SKColor.black
-        //вектор и сила гравитации
+        // вектор и сила гравитации
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        //добавляем поддержку физики
-        self.physicsBody = SKPhysicsBody(edgeChainFrom: frame as! CGPath)
-        //выклчючаем внешние воздействия на игру
+        // добавляем поддержку физики
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        // выключаем внешние воздействия на игру
         self.physicsBody?.allowsRotation = false
-        //включаем отображение отладочной информации
+        // включаем отображение отладочной информации
         view.showsPhysics = true
-        
-        
+        // поворот против часовой стрелки
+        // создаем ноду(объект)
+        let counterClockwiseButton = SKShapeNode()
+        // задаем форму круга
+        counterClockwiseButton.path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 45, height: 45)).cgPath
+        // указываем координаты размещения
+        counterClockwiseButton.position = CGPoint(x: view.scene!.frame.minX+30, y: view.scene!.frame.minY+30)
+        // цвет заливки
+        counterClockwiseButton.fillColor = UIColor.gray
+        // цвет рамки
+        counterClockwiseButton.strokeColor = UIColor.gray
+        // толщина рамки
+        counterClockwiseButton.lineWidth = 10
+        // имя объекта для взаимодействия
+        counterClockwiseButton.name = "counterClockwiseButton"
+        // Добавляем на сцену
+        self.addChild(counterClockwiseButton)
+        // Поворот по часовой стрелке
+        let clockwiseButton = SKShapeNode()
+        clockwiseButton.path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 45, height: 45)).cgPath
+        clockwiseButton.position = CGPoint(x: view.scene!.frame.maxX-80, y: view.scene!.frame.minY+30)
+        clockwiseButton.fillColor = UIColor.gray
+        clockwiseButton.strokeColor = UIColor.gray
+        clockwiseButton.lineWidth = 10
+        clockwiseButton.name = "clockwiseButton"
+        self.addChild(clockwiseButton)
     }
     
-    //вызывается при нажатии на экран
+    // вызывается при нажатии на экран
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        // перебираем все точки, куда прикоснулся палец
+        for touch in touches {
+            // определяем координаты касания для точки
+            let touchLocation = touch.location(in: self)
+            // проверяем, есть ли объект по этим координатам, и если есть, то не наша ли это кнопка
+            guard let touchedNode = self.atPoint(touchLocation) as? SKShapeNode, touchedNode.name == "counterClockwiseButton" || touchedNode.name == "clockwiseButton" else {
+                return
+            }
+            
+            // если это наша кнопка, заливаем ее зеленым
+            touchedNode.fillColor = .green
+        }
     }
-    //вызывается при прекращении нажатия на экран
+    
+    // вызывается при прекращении нажатия на экран
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // повторяем все то же самое для действия, когда палец отрывается от экрана
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            guard let touchedNode = self.atPoint(touchLocation) as? SKShapeNode, touchedNode.name == "counterClockwiseButton" || touchedNode.name == "clockwiseButton" else {
+                return
+            }
+            //но делаем цвет снова серым
+            touchedNode.fillColor = UIColor.gray
+            // вызывается при обрыве нажатия на экран, например, если телефон принял звонок и свернет приложение
+        }
     }
-    //вызывается при обрыве нажатия на экран, например, если телефон примет звонок и свернет приложение
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
-    //вызывается при обработке кадров сцены
+    // вызывается при обработке кадров сцены
     override func update(_ currentTime: TimeInterval) {
     }
 }
